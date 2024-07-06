c3ed463cd0651f0a82dfe6c396d390c081e3ce87
slottwo <41028091+slottwo@users.noreply.github.com>
2024-07-06 19:07:10 -0300

    Aula 3: CRUD

--- my_api/app.py
+++ my_api/app.py
@@ -1,10 +1,21 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-from my_api.schemas import Message
+from my_api.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', response_model=Message)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'ola mundo'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    entity = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(entity)
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,30 @@ from fastapi.testclient import TestClient
 from my_api.app import app
 
 
-def test_root_return_ok_and_ola_mundo():
+def test_root():
     client = TestClient(app)  # Arrange
 
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'ola mundo'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'email': 'test@test.org',
+            'password': 'pwd!0123',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.org',
+        'id': 1,
+    }
